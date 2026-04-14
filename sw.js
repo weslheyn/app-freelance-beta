153d7452bdf300bda9ef55157c3ad30f5ebaa08a
weslheyn <35520050+weslheyn@users.noreply.github.com>
2026-04-14 02:21:45 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE='coco-bambu-v6-search-inline';
+self.addEventListener('install',e=>e.waitUntil(
+  caches.open(CACHE).then(c=>c.addAll([
+    './',
+    './index.html',
+    './manifest.json',
+    './script.js',
+    '/escala-garcons/',
+    '/escala-garcons/index.html',
+    '/escala-garcons/manifest.json'
+  ]).catch(()=>{}))
+));
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(
+    keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+  ))
+));
+self.addEventListener('fetch',e=>e.respondWith(
+  fetch(e.request).catch(()=>caches.match(e.request))
+));
